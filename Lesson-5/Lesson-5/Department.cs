7aa32d25cc84fab9a6d79a2b58da76153e827f99
TaoDaK1ng <4itawp@gmail.com>
2019-08-06 21:04:25 +0300

    создал интерфейс

---
+++ Lesson-5/Lesson-5/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public string Employee { get; set; }
+        public Department(string name)
+        {
+            Name = name;            
+        }
+        public Department(string name, string employee) : this(name)
+        {
+            Employee = employee;
+        }
+    }
+}
---
+++ Lesson-5/Lesson-5/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        ObservableCollection<Employee> list = new ObservableCollection<Employee>();
+        public Employee(string name, string department)
+        {
+            Name = name;
+            Department = department;
+        }
+        public void AddEmployee(Employee employee)
+        {
+            list.Add(employee);
+        }
+    }
+}
